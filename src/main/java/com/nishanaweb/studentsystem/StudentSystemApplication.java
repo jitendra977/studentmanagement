6d53309bb3d388e01192dba56f92dd8427d5223c
jitu <jitendrakhadka@Jitendras-Mac-mini.local>
2024-04-11 07:14:32 +0900

    Your commit message

--- src/main/java/com/nishanaweb/studentsystem/StudentSystemApplication.java
+++ src/main/java/com/nishanaweb/studentsystem/StudentSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentsystemApplication {
+public class StudentSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentsystemApplication.class, args);
+		SpringApplication.run(StudentSystemApplication.class, args);
 	}
 
 }
